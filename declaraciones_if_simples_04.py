37d6c44fe023f18598ed6bdb662f79b84e39ccf0
efrain14 <efrain14@gmail.com>
2025-08-06 22:40:39 -0400

    el ultimo comit de la noche del 6 agosto 2025

---
+++ declaraciones_if_simples_04.py
@@ -0,0 +1,7 @@
+""""alien_color"""
+color_alien = "verde"
+if color_alien == "verde":
+    print("acabas de ganar 5 Ptos.")
+color_alien = "verde"
+if color_alien == "rojo":
+    print("acabas de ganar 5 Ptos.")
