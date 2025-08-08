269440bd141fe7dd4ba36c4bcd6c1bb20a0e35e1
efrain14 <efrain14@gmail.com>
2025-08-08 00:05:14 -0400

    el primer commit del dia 8 ago en la madrugada

--- declaraciones_if_simples_05.py
+++ declaraciones_if_simples_05.py
@@ -1,2 +1,17 @@
 """crear una lista de ingredientes solicitados por el cliente y usando un bucle
 para anunciar cada ingrediente a medida que se añade a la pizza:"""
+ingredientes_adicionales = ["champiniones", "pepperoni", "mozarela", "aceitunas"]
+for ingrediente_adicional in ingredientes_adicionales:
+    print(f"Adicional {ingrediente_adicional}.")
+print("\n has finalizado el pedido de tu pizza!")
+# si la pizzeria se queda sin aceitunas se realiza una declaracion IF dentro del
+# bucle FOR puede manejar la situacion
+print("\n")
+ingredientes_adicionales2 = ["pepperoni", "aceitunas", "mozarela"]
+for ingrediente_adiciona1 in ingredientes_adicionales2:
+    if ingrediente_adiciona1 == "aceitunas":
+        print("lo sentimos hoy no contamos con aceitunas")
+    else:
+        print(f"Adicional {ingrediente_adiciona1}.")
+print("\n has finalizado el pedido de tu pizza!")
+# pagina 177 y 86
