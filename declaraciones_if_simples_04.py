e15e6491da6a3d0d38d77ee938e12e3cf7e75f0c
efrain14 <efrain14@gmail.com>
2025-08-07 23:40:56 -0400

    el ultimo commit del dia 07 agosto 2025 en la noche

--- declaraciones_if_simples_04.py
+++ declaraciones_if_simples_04.py
@@ -5,3 +5,27 @@ if color_alien == "verde":
 color_alien = "verde"
 if color_alien == "rojo":
     print("acabas de ganar 5 Ptos.")
+else:
+    print("acabas de ganar 10 Ptos.")
+print("\n")
+color_alien = "rojo"
+if color_alien == "verde":
+    print("acabas de ganar 5 Ptos.")
+elif color_alien == "amarillo":
+    print("acabas de ganar 15 Ptos.")
+else:
+    print("acabas de ganar 10 Ptos.")
+print("\n")
+edad = 15
+if edad < 2:
+    print("la persona es un bebe")
+elif edad >= 2 and edad <= 4:
+    print("la persona es un niño pequeño")
+elif edad > 4 and edad <= 13:
+    print("la persona es un niño")
+elif edad > 13 and edad <= 20:
+    print("la persona es un adolecente")
+elif edad > 20 and edad <= 65:
+    print("la persona es un adulto")
+else:
+    print("la persona es una persona mayor")
---
+++ declaraciones_if_simples_05.py
@@ -0,0 +1,2 @@
+"""crear una lista de ingredientes solicitados por el cliente y usando un bucle
+para anunciar cada ingrediente a medida que se añade a la pizza:"""
