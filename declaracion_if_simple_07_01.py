feb901d853ac13073ce807ae30a6255c668cfe5e
efrain14 <efrain14@gmail.com>
2025-08-11 13:35:32 -0400

    ultimo commit de la oficina el 11 de ago en la tarde

--- declaracion_if_simple_07_01.py
+++ declaracion_if_simple_07_01.py
@@ -3,3 +3,11 @@
 #• Si el nombre de usuario es "admin", imprime un saludo especial, como "Hola administrador, ¿desea ver un informe de estado?".
 #• De lo contrario, imprime un saludo genérico, como "Hola Jaden, gracias por volver a iniciar sesión".
 nombres = ["jose", "carlos", "admin", "luis", "pedro"]
+for nombre in nombres:
+    print(f"bienvenido sr.{nombre}")
+print("\n")
+for nombre in nombres:
+    if nombre == "admin":   
+        print(f"Hola {nombre} desea ver un informe del estado?")
+    else:
+        print(f"Hola {nombre}, gracias por volver a iniciar sesion")
