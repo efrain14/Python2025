dd207f62773f122f899a7f6d7ef9670eff77620f
efrain14 <efrain14@gmail.com>
2025-08-10 23:44:48 -0400

    el ultimo commit del dia 10 ago 2025 en la noche

---
+++ declaracion_if_simple_07.py
@@ -0,0 +1,5 @@
+"""ejercicio"""
+#Hola administrador: Crea una lista de cinco o más nombres de usuario, incluyendo "admin". Imagina que estás escribiendo código que imprimirá un saludo para cada usuario después de iniciar sesión en un sitio web. Recorre la lista e imprime un saludo para cada usuario.
+#• Si el nombre de usuario es "admin", imprime un saludo especial, como "Hola administrador, ¿desea ver un informe de estado?".
+#• De lo contrario, imprime un saludo genérico, como "Hola Jaden, gracias por volver a iniciar sesión".
+nombres = ["jose", "carlos", "admin", "luis", "pedro"]
