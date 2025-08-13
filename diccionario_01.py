1ff6d71a64a3ddbd30f61e20915c863b699a1f58
efrain14 <efrain14@gmail.com>
2025-08-13 08:49:35 -0400

    primer comit del dia 13 ago en la manana

---
+++ diccionario_01.py
@@ -0,0 +1,10 @@
+alien_0 = {"color": "verde", "puntos": 5}
+print(alien_0["color"])
+print(alien_0["puntos"])
+nuevo_punto = alien_0["puntos"]
+print(f"acabas de ganar {nuevo_punto} nuevos puntos!")
+print("\n")
+
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
