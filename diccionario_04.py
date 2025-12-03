edadb876e5651d1b15d23449344feab22642312d
efrain14 <efrain14@gmail.com>
2025-12-03 13:24:14 -0400

    ultimo comit del dia 03122025

--- diccionario_04.py
+++ diccionario_04.py
@@ -1,7 +1,7 @@
 """   rastreo de extraterrestre """
 # Para un ejemplo más interesante, rastreemos la posición de un extraterrestre que puede moverse a diferentes velocidades. Almacenaremos un valor que represente su velocidad actual y lo usaremos para determinar cuánto debe moverse a la derecha:
 
-alien_0 = {"x_posicion" : 0, "y_posicion" : 25, "velocidad" : "mediana"}
+alien_0 = {"x_posicion" : 0, "y_posicion" : 25, "velocidad" : "rapida"}
 print(f"posicion original : {alien_0['x_posicion']}")
 
 # mover el alien a la derecha
@@ -11,7 +11,7 @@ if alien_0["velocidad"] == "lenta":
     x_incrementa = 1
 elif alien_0["velocidad"] == "mediana":
     x_incrementa = 2
-else: x_incrementa = 3
-
+else : x_incrementa = 3
+alien_0["velocidad"] == "rapida"
 alien_0["x_posicion"] = alien_0["x_posicion"] + x_incrementa
 print (f'nueva posicion: {alien_0["x_posicion"]}')
---
+++ diccionario_05.py
@@ -0,0 +1,8 @@
+lenguajes_favoritos = {
+    "jose" : "pithon",
+    "pedro" : "rust",
+    "maria" : "java",
+    "alexander" : "PHP",
+    }
+lenguaje = lenguajes_favoritos ["jose"].title()
+print(f"el lenguaje favorito de jose es {lenguaje}.")
