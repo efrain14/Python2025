6c471f631f3112b9b58ef2169b816dbfc32171d5
efrain14 <efrain14@gmail.com>
2025-12-11 12:47:26 -0400

    ultimo commit del dia 11122025

--- diccionario_13.py
+++ diccionario_13.py
@@ -11,7 +11,7 @@ for alien in aliens[0:3]:
         alien["color"] = "amarillo"
         alien["velocidad"] = "media"
         alien["puntos"] = 10
-    elif alien["color"] == "verde":
+    elif alien["color"] == "                                        verde":
         alien["color"] = "rojo"
         alien["velocidad"] = "rapida"
         alien["puntos"] = 15
