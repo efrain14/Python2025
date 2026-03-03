521689f370971dd24e3951d4f08b05d6d2e414ef
efrain14 <efrain14@gmail.com>
2026-03-03 13:09:25 -0400

    modificacion de 2 archivos el dia 03032026

--- bucle_while_03.py
+++ bucle_while_03.py
@@ -34,15 +34,15 @@ while active :
         print(mensaje)
         
         
-""" Este programa tiene el mismo resultado que el ejemplo anterior
-donde colocamos la prueba condicional directamente en la
-declaraciön while. Pero ahora que tenemos una bandera para indicar
-si el programa general estå en un estado activo, seria fåcil agregar
-mås pruebas (como declaraciones elif) para eventos que deberian
-hacer que active se convierta en False. Esto es ütil en programas
-complicados como juegos, en los que puede haber muchos eventos
-que deberian hacer que el programa deje de ejecutarse. Cuando
-cualquiera de estos eventos hace que la bandera activa se convierta
-en False, se cerrarå el bucle principal del juego, se mostrarå un
-mensaje de Game Over y se le podrå dar al jugador la opci6n de
-jugar nuevamente."""
+#  Este programa tiene el mismo resultado que el ejemplo anterior
+# donde colocamos la prueba condicional directamente en la
+# declaraciön while. Pero ahora que tenemos una bandera para indicar
+# si el programa general estå en un estado activo, seria fåcil agregar
+# mås pruebas (como declaraciones elif) para eventos que deberian
+# hacer que active se convierta en False. Esto es ütil en programas
+# complicados como juegos, en los que puede haber muchos eventos
+# que deberian hacer que el programa deje de ejecutarse. Cuando
+# cualquiera de estos eventos hace que la bandera activa se convierta
+# en False, se cerrarå el bucle principal del juego, se mostrarå un
+# mensaje de Game Over y se le podrå dar al jugador la opci6n de
+# jugar nuevamente."""
--- bucle_while_05.py
+++ bucle_while_05.py
@@ -14,9 +14,9 @@ while cuenta_numeros < 10 :
     
     print(cuenta_numeros)
     
-    """Primero, configuramos current_number en 0. Como es menor que 10, Python ingresa al bucle while. Una vez dentro del ciclo,  incrementamos el recuento en 1 por lo que
-current number es 1. La instrucciön if luego verifica el m6dulo de
-current _ number y 2. Si el m6dulo es 0 (lo que significa current_number es divisible por 2), la
-declaraciön continue le dice a Python que ignore el resto del bucle y
-volver al principio. Si el nümero actual no es divisible por 2, se
-ejecuta el resto del ciclo y Python imprime el nümero actual:"""
+#     Primero, configuramos current_number en 0. Como es menor que 10, Python ingresa al bucle while. Una vez dentro del ciclo,  incrementamos el recuento en 1 por lo que
+# current number es 1. La instrucciön if luego verifica el m6dulo de
+# current _ number y 2. Si el m6dulo es 0 (lo que significa current_number es divisible por 2), la
+# declaraciön continue le dice a Python que ignore el resto del bucle y
+# volver al principio. Si el nümero actual no es divisible por 2, se
+# ejecuta el resto del ciclo y Python imprime el nümero actual:
