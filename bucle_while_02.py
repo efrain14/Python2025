87a067779ded871fa4bfd2fde780dc4c4779a2d0
efrain14 <efrain14@gmail.com>
2026-03-23 12:48:05 -0400

    ultimo commit del dia 23032026 al mediodia

--- bucle_while_02.py
+++ bucle_while_02.py
@@ -1,4 +1,4 @@
-""" PERMITIR QUE EL USUARIO ELEIJA CUANDO SALIR 
+""" PERMITIR QUE EL USUARIO ELIJA CUANDO SALIR 
 Podemos hacer que el programa parrot.py se ejecute todo el tiempo
 que el usuario quiera poniendo la mayor parte del programa dentro
 de un bucle while. Definiremos un valor de salida y luego
--- bucle_while_03.py
+++ bucle_while_03.py
@@ -1,22 +1,8 @@
 """ BUCLE WHILE USANDO UNA BANDERA 
-En el ejemplo anterior, hicimos que el programa realizara ciertas tareas mientras
-una condición determinada era verdadera. Pero ¿qué pasa con los programas más 
-complicados en los que muchos eventos diferentes podrían provocar que el programa 
-deje deejecutarse? Por ejemplo, en un juego, varios eventos diferentes pueden 
-finalizar el juego. Cuando el jugador se queda sin barcos, se le acaba el tiempo
-o las ciudades que se suponía que debían proteger son destruidas, el juego debería 
-terminar. Debe terminar si ocurre alguno de estos eventos. Si pueden ocurrir muchos
-eventos posibles para detener el programa, intentar probar todas estas condiciones 
-en una declaración while se vuelve complicado y difícil. Para un programa que debe
-ejecutarse sólo mientras se cumplan muchas condiciones, puede definir una variable 
-que determine si todo el programa está activo o no. Esta variable, llamada bandera,
-actúa como una señal para el programa. Podemos escribir nuestros programas para que
-se ejecuten mientras el indicador esté establecido en True y dejen de ejecutarse
-cuando cualquiera de varios eventos establezca el valor del indicador en False. 
-Como resultado, nuestra declaraci6n general while necesita verificar solo una 
-condiciön: si la bandera es actualmente TRUE Luego, todas nuestras Otras pruebas 
-(para ver si ocurriö un evento que deberia establecer el indicador en False) se 
-pueden organizar claramente en el resto del programa.
+En el ejemplo anterior, hicimos que el programa realizara ciertas tareas mientras una condición determinada era verdadera. Pero ¿qué pasa con los programas más  complicados en los que muchos eventos diferentes podrían provocar que el programa  deje deejecutarse? Por ejemplo, en un juego, varios eventos diferentes pueden  finalizar el juego. Cuando el jugador se queda sin barcos, se le acaba el tiempo o las ciudades que se suponía que debían proteger son destruidas, el juego debería terminar. Debe terminar si ocurre alguno de estos eventos. Si pueden ocurrir muchos
+eventos posibles para detener el programa, intentar probar todas estas condiciones en una declaración while se vuelve complicado y difícil. Para un programa que debe ejecutarse sólo mientras se cumplan muchas condiciones, puede definir una variable  que determine si todo el programa está activo o no. Esta variable, llamada bandera,
+actúa como una señal para el programa. Podemos escribir nuestros programas para que se ejecuten mientras el indicador esté establecido en True y dejen de ejecutarse cuando cualquiera de varios eventos establezca el valor del indicador en False.  Como resultado, nuestra declaraci6n general while necesita verificar solo una 
+condiciön: si la bandera es actualmente TRUE Luego, todas nuestras Otras pruebas  (para ver si ocurriö un evento que deberia establecer el indicador en False) se pueden organizar claramente en el resto del programa.
 Agreguemos una bandera a parrot.py de la secci6n anterior. Este
 indicador, al que llamaremos ACTIVE (aunque puedes llamarlo como
 quieras), controlarå si el programa debe continuar ejecutåndose o
