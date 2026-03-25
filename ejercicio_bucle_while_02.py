2cfe63936e2ad3065e70da62c09404ca2940aad5
efrain14 <efrain14@gmail.com>
2026-03-24 22:38:30 -0400

    commit de casa el 24032026en la noche

--- ejercicio_bucle_while_02.py
+++ ejercicio_bucle_while_02.py
@@ -1,6 +1,9 @@
 """              7-5. Entradas de cine: 
 una sala de cine cobra diferentes precios de entradas segün la
-edad de la persona. Si es menor de 3 años, la entrada es gratuita; si tienen entre 3 y 12 años, el boleto cuesta $10; y si son mayores de 12 años la entrada cuesta $15. Escribe un bucle en el que preguntes a los usuarios su edad y luego les digas el coste de su entrada al cine."""
+edad de la persona. Si es menor de 3 años, la entrada es gratuita; 
+si tienen entre 3 y 12 años, el boleto cuesta $10; y si son mayores de 12 años
+la entrada cuesta $15. Escribe un bucle en el que preguntes a los usuarios su 
+edad y luego les digas el coste de su entrada al cine."""
 
 prompt = "\nque edad tienes "
 
