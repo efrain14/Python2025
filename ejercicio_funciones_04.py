a35eee841b4a66eecbb5643ea5f08722b7d41fcc
efrain14 <efrain14@gmail.com>
2026-04-16 13:09:10 -0400

    ultimo commit del dia 16042026 de la oficina

---
+++ ejercicio_funciones_04.py
@@ -0,0 +1,6 @@
+"""PRUEBELO USTED MISMO  FUNCIONES  8-4 
+Camisas grandes: modifique la funcion elabore_camiseta() para que las camisas sean grandes por defecto con un mensaje que diga Me encanta Python. Haz una camiseta grande y una camiseta mediana con el mensaje predeterminado, y una camiseta de cualquier talla con un mensaje diferente."""
+def elabore_camisetas(talla, texto) :
+    """funcion para mostrar la talla de camisetas y la frase escrita en ellas"""
+
+    
