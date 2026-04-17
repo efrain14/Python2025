cd3648aef7cffd830baa24f37d30c1021407453f
efrain14 <efrain14@gmail.com>
2026-04-17 13:09:33 -0400

    ultimo commit de la ofician el 17042026 al mediodia

---
+++ funciones_08.py
@@ -0,0 +1,8 @@
+"""VALORES DE RETORNO  Devolver un valor simple,
+veamos una funcion que toma un nombre y apellido y devuelve un nombre completo perfectamente formateado """
+
+def nombre_formateado(primer_nombre, apellido) :
+    """funcion que formatea un nombre completo"""
+    nombre_completo = f"{primer_nombre} {apellido}"
+    return nombre_completo.title()
+nombre_formateado("junior", "calderon")
