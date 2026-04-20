1118ff048da94dd5ec98b9de94deb4fe94a65a9e
efrain14 <efrain14@gmail.com>
2026-04-19 23:52:24 -0400

    ultimo commit del dia en la casa 19042026 noche

--- funciones_11.py
+++ funciones_11.py
@@ -3,4 +3,11 @@ una funcion puede devolver cualquier tipo de valor que necesite, incluidas
 estructuras de datos mas complicadas como listas y diccionarios, por ejemplo la 
 siguiente funcion toma partes de un nombre y devuelve un diccionario que representa
 una persona"""
-def construlle_persona() :
+def construlle_persona(primer_nombre, segundo_nombre) :
+    """retorna un diccionario con la informacion de una persona"""
+    persona = {"primero" : primer_nombre, "segundo" : segundo_nombre}
+    return persona
+
+trabajador = construlle_persona("pedro", "briceño")
+print(trabajador)
+    
